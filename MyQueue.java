eb4dd9f0b463120a0a1032f9cf35b83c6766d69f
Guangjin Liu <35760992+GuangjinLiu@users.noreply.github.com>
2020-02-27 00:52:24 -0800

    Add files via upload

---
+++ MyQueue.java
@@ -0,0 +1,56 @@
+public class MyQueue<T> extends MyList<T> {
+
+    public MyQueue<T> enQueue(T obj) {
+        //addToRear(obj);
+        Node node = new Node(obj);
+        if(tail == null) {
+            head = node;
+            tail = node;
+        }
+        else {
+            tail.next = node;
+            tail = node;
+        }
+        return this;
+    }
+
+    public MyQueue<T> deQueue() {
+        //removeFront();
+        if(head == null) {
+            System.out.println("Queue is empty!");
+        }
+        else {
+            Node temp = head;
+            head = temp.next;
+            //head.prev = null;
+            temp = null;
+        }
+        return null;
+    }
+
+    public MyQueue<T> display() {
+        if(head == null) {
+            System.out.println("Queue is empty!");
+        }
+        else {
+            Node temp = head;
+            while(temp.next != null) {
+                System.out.println(temp.data);
+            }
+        }
+        return this;
+    }
+
+    public MyQueue<T> front() {
+        if(head == null) {
+            System.out.println(head);
+            System.out.println("Queue is empty!");
+        }
+        else {
+            Node temp = head;
+            System.out.println(temp.data);
+            temp = null;
+        }
+        return this;
+    }
+}
